2edb95d067121d676c367e768d866151211d756b
Hoanvuonq <hoanvuonq2002@gmail.com>
2023-03-20 13:54:32 +0700

    GUI 30%

--- face-recognition/src/App.js
+++ face-recognition/src/App.js
@@ -1,23 +1,21 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import './Scss/main.scss';
+
+// Components //
+import Home from './Components/Home/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload. 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Fragment>
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                </Routes>
+            </Fragment>
+        </BrowserRouter>
+    );
 }
 
 export default App;
